8fb5a601e39ecc219d137e86907e181490475a67
Gerard van Helden <gerard@zicht.nl>
2016-06-08 18:11:25 +0200

    first commit

---
+++ j2-lint.py
@@ -0,0 +1,39 @@
+"""
+@author Gerard van Helden <gerard@zicht.nl>
+@license DBAD, see <http://www.dbad-license.org/>
+
+Simple j2 linter, useful for checking jinja2 template syntax
+"""
+import jinja2
+import os.path
+
+
+class AbsolutePathLoader(jinja2.BaseLoader):
+    def get_source(self, environment, path):
+        if not os.path.exists(path):
+            raise jinja2.TemplateNotFound(path)
+        mtime = os.path.getmtime(path)
+        with file(path) as f:
+            source = f.read().decode('utf-8')
+        return source, path, lambda: mtime == os.path.getmtime(path)
+
+
+def check(template, out, err):
+    env = jinja2.Environment(loader=AbsolutePathLoader())
+
+    try:
+        env.get_template(template)
+        out.write("%s: Syntax OK\n" % template)
+        return 0
+    except jinja2.exceptions.TemplateSyntaxError as e:
+        err.write("%s: Syntax check failed: %s in %s at %d\n" % (template, e.message, e.filename, e.lineno))
+        return 1
+
+
+if __name__ == "__main__":
+    import sys
+
+    try:
+        sys.exit(reduce(lambda r, fn: r + check(fn, sys.stdout, sys.stderr), sys.argv[1:], 0))
+    except IndexError:
+        print "Usage: j2-lint.py filename\n"
