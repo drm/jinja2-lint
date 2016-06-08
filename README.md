ffe4bb092addffed4916dad12944e8a43b1452b2
Gerard van Helden <gerard@zicht.nl>
2016-06-08 18:18:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Linter for jinja2 #
+
+Every now and then, it's pretty useful to just have a cli tool that does the job. This does it.
+
+## Usage: ##
+```
+python j2-lint.py my-template.j2
+```
+
+It accepts multiple arguments so shell expansion and/or combining with find is no issue:
+
+```
+python j2-lint.py *.j2
+find src -type f -name "*.j2" -exec python j2-lint.py '{}' +
+```
+
--- j2-lint.py
+++ j2-lint.py
@@ -6,6 +6,7 @@ Simple j2 linter, useful for checking jinja2 template syntax
 """
 import jinja2
 import os.path
+from functools import reduce
 
 
 class AbsolutePathLoader(jinja2.BaseLoader):
@@ -13,8 +14,8 @@ class AbsolutePathLoader(jinja2.BaseLoader):
         if not os.path.exists(path):
             raise jinja2.TemplateNotFound(path)
         mtime = os.path.getmtime(path)
-        with file(path) as f:
-            source = f.read().decode('utf-8')
+        with open(path) as f:
+            source = f.read()
         return source, path, lambda: mtime == os.path.getmtime(path)
 
 
@@ -25,6 +26,9 @@ def check(template, out, err):
         env.get_template(template)
         out.write("%s: Syntax OK\n" % template)
         return 0
+    except jinja2.TemplateNotFound:
+        err.write("%s: File not found\n" % template)
+        return 2
     except jinja2.exceptions.TemplateSyntaxError as e:
         err.write("%s: Syntax check failed: %s in %s at %d\n" % (template, e.message, e.filename, e.lineno))
         return 1
@@ -36,4 +40,4 @@ if __name__ == "__main__":
     try:
         sys.exit(reduce(lambda r, fn: r + check(fn, sys.stdout, sys.stderr), sys.argv[1:], 0))
     except IndexError:
-        print "Usage: j2-lint.py filename\n"
+        sys.stdout.write("Usage: j2-lint.py filename\n")
