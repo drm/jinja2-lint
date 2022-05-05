78f0887392b99239b006ba3761d90cdfa35cd49d
Jens Heinrich <github.com/JensHeinrich>
2022-05-05 16:03:12 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 setup(name='jinja2-lint',
       version='1.0',
+      py_modules=['j2lint'],
       install_requires=['jinja2'],
       entry_points={
         'console_scripts': [
